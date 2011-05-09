03631836ac5878d045e05762ad697dd501e1299f
concas.davide@gmail.com <concas.davide@gmail.com@38086d82-aef0-9ef7-b7d7-0edfc06920ca>
2011-05-09 09:15:08 +0000

    First commit group 4

---
+++ trunk/group4/Cinema-ejb/src/java/ejb/GestoreFilm.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import java.util.Iterator;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import java.util.List;
+
+/**
+ * questo è un session bean, qui ci stanno dei metodi pubblici per gestire il tutto
+ * @author dave
+ */
+@Stateless
+public class GestoreFilm implements GestoreFilmLocal {
+    @EJB
+    private FilmFacadeLocal filmFacade;
+    @Override
+    public void addFilm(String titolo, String regista){
+        Film f = new Film();
+        f.setTitoloFilm(titolo);
+        f.setRegistaFilm(regista);
+        filmFacade.create(f);
+    }
+   @Override
+    public String listFilm(){
+        List<Film>  listaFilm = filmFacade.findAll();
+        Film iterator;
+        String ret = "";
+        for(int i=0; i< listaFilm.size();i++){
+            iterator = listaFilm.get(i);
+            ret +=  iterator.titolo;
+            ret +=  " -- ";
+            ret += iterator.regista;
+            ret +="</br>";
+            
+        }
+        return ret;
+    }
+    
+}
